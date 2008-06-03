6c1a06dc512600c5055275fc9e90401cf7984a12
Clayton Parker <me@claytron.com>
2008-06-03 00:38:25 +0000

    require some packages in order for this to work

--- setup.py
+++ setup.py
@@ -26,6 +26,8 @@ setup(name='sixfeetup.workflow.featured',
       install_requires=[
           'setuptools',
           # -*- Extra requirements: -*-
+          'sixfeetup.workflow.chained',
+          'sixfeetup.catalogoverrides',
       ],
       entry_points="""
       # -*- Entry points: -*-
